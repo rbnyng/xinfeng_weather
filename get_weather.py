3373000f25fba1b97e0ba0b298e9bf770b948f32
Robin Young <59581322+rbnyng@users.noreply.github.com>
2023-11-17 19:59:42 -0500

    Update get_weather.py

--- get_weather.py
+++ get_weather.py
@@ -41,7 +41,7 @@ def append_to_csv(new_data, filename):
 
     if os.path.isfile(filename):
         existing_data = pd.read_csv(filename)
-        updated_data = pd.concat([existing_data, new_data], ignore_index=True)
+        updated_data = pd.concat([new_data, existing_data], ignore_index=True)
         updated_data = updated_data.astype({'觀測時間': 'str'})
         updated_data = updated_data.drop_duplicates()
     else:
